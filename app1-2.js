25d50bcc641b7a6cf4ccd7122f1fcb904cab785a
asao112 <86589325+asao112@users.noreply.github.com>
2021-06-29 18:15:56 +0900

    Update app1-2.js
    Co-authored-by: Takayuki Miyazaki <takayuki.miyazaki@sansan.com>

--- app1-2.js
+++ app1-2.js
@@ -1,7 +1,7 @@
 var question=0;
 var answers=true;
     while(true){
-    if(answers=(prompt("日本の首都は？")!== "東京")){
+    if (answers === (prompt("日本の首都は？") !== "東京")) {
     alert("不正解です！");
     continue;
     }

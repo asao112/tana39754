854736d5ac17b7e431fe10014992131e8868575f
asao112 <86589325+asao112@users.noreply.github.com>
2021-06-30 18:38:11 +0900

    Update app1-2.js

--- app1-2.js
+++ app1-2.js
@@ -1,9 +1,9 @@
-var question=0;
-var answers=true;
-    while(true){
+const quiz =['question','answears'];
+const answers=true;
+while (true) {
     if (answers === (prompt("日本の首都は？") !== "東京")) {
-    alert("不正解です！");
-    continue;
+        alert("不正解です！");
+        continue;
     }
-    alert("正解です！")
-    }
+    alert("正解です！");
+}
